d96440eacf09fd485ab2e5bf9824dafeb1c86978
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-04-01 23:56:35 +0100

    fixed socket connection

--- context/socket.tsx
+++ context/socket.tsx
@@ -37,7 +37,7 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
 export const useSocket = () => {
   const context = useContext(SocketContext);
   if (context === undefined) {
-    throw new Error('useSocket must be used within a SocketProvider');
+    throw new Error('useSocket mut be used within a SocketProvider');
   }
   return context.socket;
 };
