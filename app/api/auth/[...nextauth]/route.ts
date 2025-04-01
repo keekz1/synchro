7b870174206d89efd6e3f32364a5f0bcf93ef550
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-04-01 15:48:53 +0100

    adde folders

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,11 @@
-export {GET , POST} from "@/auth"
+import GoogleProvider from "next-auth/providers/google";
+
+export default {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
