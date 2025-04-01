fafea18b5ded611bdc07b3bf8d89570f6dc19f93
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-04-01 15:33:57 +0100

    adde folders

--- routes.ts
+++ routes.ts
@@ -18,8 +18,8 @@ export const publicRoutes = [
  * @types{string[]}
  */
 export const authRoutes = [
-    "/map",
-    "/profile",
+   " https://synchro-kappa.vercel.app/map",
+   "https://synchro-kappa.vercel.app/profile",
 
     "/auth/login",
     "/auth/register",
