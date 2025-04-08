3527c73b1fb3134edcab100c1f089d7b26cf1e44
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-04-08 15:41:22 +0100

    password reset update

--- app/auth/new-password/page.tsx
+++ app/auth/new-password/page.tsx
@@ -1,11 +1,12 @@
+import { Suspense } from "react";
 import { NewPasswordForm } from "@/components/auth/new-password-form";
 
+const NewPasswordPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NewPasswordForm />
+    </Suspense>
+  );
+};
 
-const NewPassowrdPage = () =>{
-    return(
-<NewPasswordForm/>
-
-    );
-}
-
-export  default NewPassowrdPage;
+export default NewPasswordPage;
