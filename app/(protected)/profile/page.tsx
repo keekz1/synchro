06154d0b4d14ce5ae84d40fbf2e967d95ecfe31f
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-04-11 20:49:10 +0100

    color update

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -28,8 +28,8 @@ export default function ProfilePage() {
   // Skeleton Loader
   if (!user) {
     return (
-      <div className="flex flex-col items-center mt-10 space-y-4">
-        <h1 className="text-2xl font-bold">Profile Page</h1>
+<div className="h-screen w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_#99f6e4,_#134e4a)] from-teal-300 to-teal-900">
+<h1 className="text-2xl font-bold">Profile Page</h1>
         <div className="w-32 h-32 bg-gray-300 rounded-full animate-pulse"></div>
         <div className="w-48 h-6 bg-gray-300 rounded animate-pulse"></div>
         <div className="w-64 h-4 bg-gray-300 rounded animate-pulse"></div>
