6797667c668ba4a553701baca8fa6815bcbfeb91
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-05-01 22:55:21 +0100

    delete accoun function added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,8 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
    {
-    files: ["lib/prisma.ts"],
+    files: ["lib/prisma.ts",      "lib/db.ts"  
+    ],
     rules: {
       "no-var": "off", 
       "vars-on-top": "off"  
