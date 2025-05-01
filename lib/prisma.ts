132cb9e0f519d31ef285515040b7e9a49ceffdc9
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-05-01 22:44:37 +0100

    delete accoun function added

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
- 
-declare global {
-  var prisma: PrismaClient | undefined;
+ declare global {
+   var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prisma || new PrismaClient();
+ const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
+ if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma;
 }
 
