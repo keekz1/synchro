734058ba5dc49830355ef387ffbe7e686ed7dd8c
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-05-01 22:52:37 +0100

    delete accoun function added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+   {
+    files: ["lib/prisma.ts"],
+    rules: {
+      "no-var": "off", 
+      "vars-on-top": "off"  
+    }
+  }
 ];
 
 export default eslintConfig;
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-// eslint-disable-next-line no-var
 declare global {
-  var prisma: PrismaClient | undefined;
+  
+ var prisma: PrismaClient | undefined;  
 }
 
-const prisma = globalThis.prisma || new PrismaClient();
+export const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma;
 }
-
-export { prisma };
